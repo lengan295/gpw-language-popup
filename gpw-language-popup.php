5f054ff71ad623de6377e338856e2efd07a42946
Le Ngan <le.ngan@lundimatin.fr>
2023-10-24 12:31:31 +0700

    content_bait

--- gpw-language-popup.php
+++ gpw-language-popup.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Language Popup for GTranslate
-Description: Hiển thị một popup chứa một selector ngôn ngữ cho người dùng.
+Description: Hiển thị một popup chứa một selector ngôn ngữ cho người dùng (cần các plugin sau : gtranslate, kadence-blocks, kadence-blocks-pro)
 Version: 1.0
 Author: Le Ngan
 */
@@ -26,8 +26,8 @@ if (!function_exists("display_select_language_popup")) {
         if (get_current_user_id()) {
             return;
         }
-        $page = get_page_by_path('select-language');
-        $content = apply_filters('the_content', $page->post_content);
+        $content_bait = file_get_contents(__DIR__ . '/popup-content_bait.html');
+        apply_filters('the_content', $content_bait);
 
         $content = file_get_contents(__DIR__ . '/popup-content.html');
 
